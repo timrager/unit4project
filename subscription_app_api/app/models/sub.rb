4c7a1017dfd99840a371cdcf8f8af24ebffd519f
timrager <tim.rager@gmail.com>
2020-07-10 08:48:15 -0700

    Added Active Record relations between User and Sub

--- subscription_app_api/app/models/sub.rb
+++ subscription_app_api/app/models/sub.rb
@@ -1,2 +1,4 @@
 class Sub < ApplicationRecord
+    has_many :registers
+    has_many :users, through: :registers
 end
--- subscription_app_api/app/models/user.rb
+++ subscription_app_api/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :registers
+    has_many :subs, through: :registers
 end
