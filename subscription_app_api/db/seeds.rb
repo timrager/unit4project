a2cf375ce338906e51a0faeae3ab9c4ebae19b1c
timrager <tim.rager@gmail.com>
2020-07-10 08:30:08 -0700

    seeded db

--- subscription_app_api/db/seeds.rb
+++ subscription_app_api/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([
+    { name: "Samantha" },
+    { name: "Taylor" },
+    { name: "Jeremiah" }
+])
+
+Sub.create([
+    { name: "Spotify", price: 9.99 },
+    { name: "Hulu", price: 5.99 },
+    { name: "Netflix", price: 8.99 }
+])
+
