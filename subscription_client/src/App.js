82dbfa4104f296f260a710e15e3885fd3dcf8167
timrager <tim.rager@gmail.com>
2020-07-10 11:10:31 -0700

    rendered user with subs

--- subscription_client/src/App.js
+++ subscription_client/src/App.js
@@ -5,17 +5,17 @@ import './App.css';
 class App extends Component{
 
   state = {
-    subscriptions: []
+    users: []
   }
   
   componentDidMount() {
-    this.getSubscriptions()
+    this.getUsers()
   }
   
-  getSubscriptions = () => {
-    fetch('http://localhost:3000/subs')
+  getUsers = () => {
+    fetch('http://localhost:3000/users')
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => this.setState({ users: json }))
     .catch(error => console.log(error))
   }
 
@@ -23,9 +23,27 @@ class App extends Component{
 
 
   render(){
+    console.log(this.state.users)
     return(
       <div className="App">
         <h1>Subscription Manager</h1>
+        <div>
+          {this.state.users.map( user => {
+            return(
+              <div key={user.id} className="user">
+                <h2>{ user.name }</h2>
+                { user.subs.map( sub => {
+                  return(
+                    <div>
+                      <p>{sub.name}</p>
+                      <p>{sub.price}</p>
+                    </div>
+                  )
+                })}
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
