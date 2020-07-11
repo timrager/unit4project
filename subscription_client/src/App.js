e3375a719a811c68b44befd46afb59231ab55f7b
timrager <tim.rager@gmail.com>
2020-07-10 18:34:25 -0700

    added some CRUD functionality

--- subscription_client/src/App.js
+++ subscription_client/src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 class App extends Component{
 
   state = {
-    users: []
+    users: [],
+    formInputs: {
+      name: ''
+    }
   }
   
   componentDidMount() {
@@ -19,7 +22,70 @@ class App extends Component{
     .catch(error => console.log(error))
   }
 
+  handleChange = (event) => {
+    const updateInput = Object.assign( this.state.formInputs, 
+      { [event.target.id]: event.target.value })
+      this.setState(updateInput)
+  }
 
+  updateUser = (user) => {
+    fetch('http://localhost:3000/users/'+user.id, {
+      body: JSON.stringify(this.state.formInputs),
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(updatedUser => updatedUser.json())
+    .then(jsonedUser => {
+      this.setState({
+        formInputs: {
+          name: ''
+        },
+        users: [jsonedUser, ...this.state.users]
+      })
+    })
+  }
+
+  deleteUser = (user) => {
+    fetch('http://localhost:3000/users/'+user.id, {
+        method: 'DELETE'
+      })
+      .then(jsonedUser => {
+        this.setState({
+          formInputs: {
+            name: ''
+          },
+          users: [jsonedUser, ...this.state.users]
+        })
+      })
+  }
+
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    fetch('http://localhost:3000/users', {
+      body: JSON.stringify(this.state.formInputs),
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(createdUser => {
+      return createdUser.json()
+    })
+    .then(jsonedUser => {
+      this.setState({
+        formInputs: {
+          name: ''
+        },
+        users: [jsonedUser, ...this.state.users]
+      })
+    })
+    .catch(error => console.log(error))
+  }
 
 
   render(){
@@ -28,10 +94,22 @@ class App extends Component{
       <div className="App">
         <h1>Subscription Manager</h1>
         <div>
-          {this.state.users.map( user => {
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Add User's Name</label>
+            <input type="text" id="name" onChange={this.handleChange}/>
+            <input type="submit" className="submit" />
+          </form>
+        </div>
+        <div>
+          { this.state.users.map( (user,index) => {
             return(
               <div key={user.id} className="user">
                 <h2>{ user.name }</h2>
+                <form>
+                  <input type="text" id="name" onChange={this.handleChange} />
+                  <button onClick={()=> this.updateUser(user)}>Update</button>
+                  <button onClick={()=> this.deleteUser(user)}>Delete</button>
+                </form>
                 { user.subs.map( sub => {
                   return(
                     <div>
